82078dcc47559412931c7dfa73238e4ef6cb6a5e
Danieli-Costa <danielibibiano2016@gmail.com>
2023-02-27 17:05:33 -0800

    Installed axios and cleaned some CSS

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function App() {
+export default function Weather() {
   let weatherData = {
     city: "New York",
     temperature: 19,
@@ -14,68 +15,62 @@ export default function App() {
   return (
     <div className="Weather">
       <div className="container">
-        <div className="weather-app-wrapper">
-          <div className="weather-app">
-            <form className="search-form mb-3">
-              <div className="row">
-                <div className="col-9">
-                  <input
-                    type="search"
-                    placeholder="Enter a city"
-                    autoComplete="on"
-                    className="form-control"
-                  />
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search 🔍"
-                    className="btn btn-primary w-100"
-                  />
-                </div>
-              </div>
-            </form>
-
-            <h1>{weatherData.city}</h1>
-            <h5> {weatherData.date} </h5>
-            <h6>{weatherData.description}</h6>
-
-            <div className="row">
-              <div className="col-6">
-                <div className="clearfix weather-temperature">
-                  <img
-                    src={weatherData.imgUrl}
-                    alt={weatherData.description}
-                    className="float-left"
-                    width="100"
-                  />
-
-                  <strong className="temperature">
-                    {weatherData.temperature}
-                  </strong>
-                  <span className="units">°C</span>
-                </div>
-              </div>
-              <div className="col-6">
-                <ul>
-                  <li>Humidity: {weatherData.humidity}%</li>
-                  <li>Wind: {weatherData.wind} km/h</li>
-                </ul>
-              </div>
+        <form className="search-form mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                autoComplete="on"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search 🔍"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
+        </form>
+
+        <h1>{weatherData.city}</h1>
+        <h5> {weatherData.date} </h5>
+        <h6>{weatherData.description}</h6>
+
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+                width="100"
+              />
+
+              <span className="temperature">{weatherData.temperature}</span>
+              <span className="units">°C</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
-        <footer>
-          <a
-            href="https://github.com/Danieli-Costa/Weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>
-          , by Danieli Costa
-        </footer>
       </div>
+      <footer>
+        <a
+          href="https://github.com/Danieli-Costa/Weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by Danieli Costa
+      </footer>
     </div>
   );
 }
