709abf5a6b20cbf3a6a471ea0029c2d8cf14a7a1
Danieli-Costa <danielibibiano2016@gmail.com>
2023-08-01 15:57:06 -0700

    Deleted parts of past project to start from zero

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <footer>
+          <a
+            href="https://github.com/Danieli-Costa/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Danieli Costa
+        </footer>
+      </div>
+    </div>
+  );
+}
