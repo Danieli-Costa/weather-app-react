6567ba73c45aef2161d7176692ba9cb62c1e2370
Danieli-Costa <danielibibiano2016@gmail.com>
2023-08-31 13:19:24 -0700

    Styled search bar and made it responsive

--- src/Weather.js
+++ src/Weather.js
@@ -43,26 +43,19 @@ export default function Weather(props) {
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form onSubmit={handleSubmit} className="form">
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-                onChange={handleCityChange}
-                autoComplete="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search "
-                className="btn search-btn w-100"
-              />
-            </div>
-          </div>
-        </form>
+        <div className="row">
+          <form onSubmit={handleSubmit} className="form">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              onChange={handleCityChange}
+              autoComplete="on"
+            />
+
+            <input type="submit" value="Search " className="btn search-btn" />
+          </form>
+        </div>
         <WeatherInfo data={weatherData} unit={unit} setUnit={setUnit} />
         <WeatherForecast city={weatherData.city} unit={unit} />
       </div>
