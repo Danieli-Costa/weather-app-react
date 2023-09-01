4ed2d053c4c6f2981fde1a575c878095af814bec
Danieli-Costa <danielibibiano2016@gmail.com>
2023-08-31 18:25:31 -0700

    Fixed responsiveness (inspect tool not corresponding to phone)

--- src/Weather.js
+++ src/Weather.js
@@ -43,19 +43,26 @@ export default function Weather(props) {
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <div className="row">
-          <form onSubmit={handleSubmit} className="form">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control search-input"
-              onChange={handleCityChange}
-              autoComplete="on"
-            />
-
-            <input type="submit" value="Search " className="btn btn-primary" />
-          </form>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control search-input"
+                onChange={handleCityChange}
+                autoComplete="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search "
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
 
         <WeatherInfo data={weatherData} unit={unit} setUnit={setUnit} />
         <WeatherForecast city={weatherData.city} unit={unit} />
